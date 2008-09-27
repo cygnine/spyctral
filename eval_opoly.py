aa4b49c042cc77ee9a535b52560b667c63d5c2fc
Akil Narayan <anaray@dam.brown.edu>
2008-09-27 18:13:54 -0400

    Initial commit

---
+++ eval_opoly.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python
+# Evaluates orthogonal polynomials given the recurrence constants
+#
+# 20080927 -- acn
+
+import numpy as _np
+
+# Evaluates the monic orthogonal polynomials at x defined by the recurrence constants
+# a and b. It is assumed that a and b are long enough to evaluate the max(n)-th
+# orthogonal polynomial. 
+# The output is a length(x) by length(n) array
+def eval_opoly(x,n,a,b) :
+
+    # Preprocessing: unravel x and n arrays
+    x = _np.array(x)
+    n = _np.array(n)
+    N = _np.max(n)
+    x = x.ravel()
+    n = n.ravel()
+
+    p = _np.zeros([x.size, N+2]);
+
+    p[:,0] = 1.;
+    
+    if N==0 :
+        return p[:,n]
+
+    p[:,1] = p[:,0]*(x-a[0])
+
+    for q in range(N) :
+        p[:,q+2] = (x-a[q+1])*p[:,q+1] - b[q+1]*p[:,q]
+
+    return p[:,n]
+
+
+#for q=1:N;
+#  p(:,q+2) = (x-alpha(q+1)).*p(:,q+1) - beta(q+1)*p(:,q);
+#end
+#
+#p = p(:,n+1);
