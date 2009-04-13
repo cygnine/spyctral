33d3f9e95147a3b5cac0d38408214200ca8639be
Akil Narayan <anaray@dam.brown.edu>
2009-04-13 13:34:38 -0400

    Cleaned up FFT - In fft.py, got rid of using hstacks: instead used rolls - In connection.py, splicing s/c_expand/collapse into sc_expand and sc_collapse

--- connection.py
+++ connection.py
@@ -21,15 +21,14 @@ def int_connection(modes,g,d,G,D):
 
         N = modes.shape[0]
 
-        cmodes = c_collapse(modes,N)*1/2.
-        smodes = s_collapse(modes,N)*1/2.
+        [cmodes,smodes] = sc_collapse(modes,N)
 
         cmodes = jconnection(cmodes,d-1/2.,g-1/2.,D,G)
         smodes = jconnection(smodes,d+1/2.,g+1/2.,D,G)
 
-        return c_expand(cmodes,N) + s_expand(smodes,N)
+        return sc_expand(cmodes,smodes,N)
     else:
-        return modes
+        return modes.copy()
 
 # Reverses the integer connection performed by int_connection
 # The input modes is a vector of (g+G,d+D) modes, and we wish to demote
@@ -43,15 +42,99 @@ def int_connection_backward(modes,g,d,G,D):
 
         N = modes.size
 
-        cmodes = c_collapse(modes,N)*1/2.
-        smodes = s_collapse(modes,N)*1/2.
+        [cmodes,smodes] = sc_collapse(modes,N)
 
         cmodes = jconnection_inv(cmodes,d-1/2.,g-1/2.,D,G)
         smodes = jconnection_inv(smodes,d+1/2.,g+1/2.,D,G)
         
-        return c_expand(cmodes,N) + s_expand(smodes,N)
+        return sc_expand(cmodes,smodes,N)
+    else:
+        return modes.copy()
+
+def sc_collapse(modes,N):
+    from numpy import sqrt
+
+    tempN = N/2
+    tempN2 = tempN - ((N+1)%2)
+
+    cmodes = modes[:tempN+1][::-1].copy()
+    smodes = -cmodes[1:].copy()
+    
+    cmodes[1:tempN2+1] += modes[tempN+1:]
+    smodes[:tempN2] += modes[tempN+1:]
+    cmodes[0] *= sqrt(2)
+
+    return [cmodes*1/2.,smodes*1/2.]
+
+# Helper function: combines roles of c_expand, s_expand
+def sc_expand(cmodes,smodes,N):
+    from numpy import sqrt,zeros
+    Neven = (N+1)%2
+    n = N/2
+    factor = N - Neven
+    cmodes[0] *= sqrt(2)
+    
+    # 'positive' modes
+    pmodes = cmodes.copy()
+    pmodes[1:] += smodes
+    # Turn smodes into 'negative' modes
+    smodes = (cmodes[1:] - smodes)[::-1]
+
+    # Put it all together
+    modes = zeros(2*n+1,dtype='complex128')
+    modes[:n],modes[n:] = smodes,pmodes
+    if bool(Neven):
+        return modes[:-1]
     else:
         return modes
+
+# Returns the necessary matrices for performing the int_connection; use
+# as an overhead to plug into int_connection_online.
+def int_connection_overhead(N,g,d,G,D):
+    from jfft import rmatrix_entries as jconnection_entries
+    G = int(G)
+    D = int(D)
+
+    cconnect = jconnection_entries((N+2)/2,d-1/2.,g-1/2.,D,G)
+    sconnect = jconnection_entries(N/2,d+1/2.,g+1/2.,D,G)
+
+    return [cconnect,sconnect]
+
+# Applies the connection matrices returned by int_connection_overhead to
+# perform the fft.
+def int_connection_online(modes,matrices):
+    from jfft import rmatrix_entries_apply as jconnection_apply
+
+    if matrices[0].shape[1]>1:
+        N = modes.shape[0]
+
+        [cmodes,smodes] = sc_collapse(modes,N)
+
+        cmodes = jconnection_apply(cmodes,matrices[0])
+        smodes = jconnection_apply(smodes,matrices[1])
+
+        return sc_expand(cmodes,smodes,N)
+    else: 
+        return modes.copy()
+
+def int_connection_backward_online(modes,matrices):
+    from jfft import rmatrix_entries_invert as jconnection_apply
+
+    if matrices[0].shape[1]>1:
+        N = modes.shape[0]
+
+        [cmodes,smodes] = sc_collapse(modes,N)
+
+        cmodes = jconnection_apply(cmodes,matrices[0])
+        smodes = jconnection_apply(smodes,matrices[1])
+
+        return sc_expand(cmodes,smodes,N)
+    else: 
+        return modes.copy()
+
+
+
+###################### DEPRECATED ######################
 
 # Helper function: collapses to even modes
 def c_collapse(modes,N):
@@ -79,51 +162,3 @@ def c_expand(modes,N):
 def s_expand(modes,N):
     factor = modes.size - (1+(-1)**N)/2
     return _np.hstack((-modes[::-1],_np.array([0.]),modes[:factor]))
-
-# Returns the necessary matrices for performing the int_connection; use
-# as an overhead to plug into int_connection_online.
-def int_connection_overhead(N,g,d,G,D):
-    from jfft import rmatrix_entries as jconnection_entries
-    G = int(G)
-    D = int(D)
-
-    cmodes = jconnection_entries((N+2)/2,d-1/2.,g-1/2.,D,G)
-    smodes = jconnection_entries(N/2,d+1/2.,g+1/2.,D,G)
-
-    return [cmodes,smodes]
-
-# Applies the connection matrices returned by int_connection_overhead to
-# perform the fft.
-def int_connection_online(modes,matrices):
-    from jfft import rmatrix_entries_apply as jconnection_apply
-
-    if matrices[0].shape[1]>1:
-        N = modes.shape[0]
-
-        modes *= 1/2.
-        cmodes = c_collapse(modes,N)
-        smodes = s_collapse(modes,N)
-
-        cmodes = jconnection_apply(cmodes,matrices[0])
-        smodes = jconnection_apply(smodes,matrices[1])
-
-        return c_expand(cmodes,N) + s_expand(smodes,N)
-    else: 
-        return modes
-
-def int_connection_backward_online(modes,matrices):
-    from jfft import rmatrix_entries_invert as jconnection_apply
-
-    if matrices[0].shape[1]>1:
-        N = modes.shape[0]
-
-        modes *= 1/2.
-        cmodes = c_collapse(modes,N)
-        smodes = s_collapse(modes,N)
-
-        cmodes = jconnection_apply(cmodes,matrices[0])
-        smodes = jconnection_apply(smodes,matrices[1])
-
-        return c_expand(cmodes,N) + s_expand(smodes,N)
-    else: 
-        return modes
--- fft.py
+++ fft.py
@@ -5,25 +5,20 @@
 __all__ = []
 
 # Uses numpy fft to obtain modes. Only works on 1D arrays
-# DOES NOT DO G,D != 0 YET
 def fft(fx,g=0,d=0,scale=1):
-    from numpy import sqrt, hstack, arange, exp, sign
+    from numpy import sqrt, roll, arange, exp, sign
     from numpy.fft import fft as ft
     from scipy import pi
+    from quad import N_to_ks
 
     from connection import int_connection
 
     N = fx.size
     modes = ft(fx)*sqrt(2*pi)/N
 
-    if bool(N%2):
-        n = (N-1)/2
-        modes = hstack((modes[-n:],modes[:(n+1)]))
-        ks = arange(-n,n+1)
-    else:
-        n = N/2
-        modes = hstack((modes[-n:],modes[:n]))
-        ks = arange(-n,n)
+    ks = N_to_ks(N)
+
+    modes = roll(modes,N/2)
 
     flags = ks!=0
     flags2 = ((ks%2)==1)
@@ -31,12 +26,11 @@ def fft(fx,g=0,d=0,scale=1):
     modes[flags2] *= -1
 
     return int_connection(modes,0,0,g,d)
-    #return modes
 
 # Uses numpy ifft to obtain nodes. Only works on 1D arrays
 # Just reverses what was done in fft above
 def ifft(f,g=0.,d=0.,scale=1.):
-    from numpy import sqrt, hstack, arange, exp, sign
+    from numpy import sqrt, roll, arange, exp, sign
     from numpy.fft import ifft as ift
     from scipy import pi
     from quad import N_to_ks
@@ -44,7 +38,6 @@ def ifft(f,g=0.,d=0.,scale=1.):
 
     N = f.size
     ks = N_to_ks(N)
-    fx = f.copy()
     fx = int_connection_backward(f,0,0,g,d)
 
     flags = ks!=0
@@ -53,14 +46,8 @@ def ifft(f,g=0.,d=0.,scale=1.):
     fx[flags2] *= -1
     fx[flags] /= exp(-1j*ks[flags]*pi/N)
 
-    if bool(N%2):
-        n = (N-1)/2
-        fx = hstack((fx[-(n+1):],fx[:n]))
-    else:
-        n = N/2
-        fx = hstack((fx[-n:],fx[:n]))
+    fx = roll(fx,-(N-1)/2)
 
-    #return N/sqrt(2*pi)*ift(fx)/sqrt(scale)
     return N/sqrt(2*pi)*ift(fx)
 
 # Combines overhead calculations needed to perform fft
@@ -91,21 +78,16 @@ def fft_overhead(N,g=0.,d=0.,scale=1.):
 # Does the direct FFT using the overhead calculations from fft_overhead
 def fft_online(fx,overhead):
     from numpy.fft import fft as ft
-    from numpy import hstack
+    from numpy import roll
     from connection import int_connection_online as int_connection
 
     modes = ft(fx)
     N = fx.size
-    if bool(N%2):
-        n = (N-1)/2
-        modes = hstack((modes[-n:],modes[:(n+1)]))
-    else:
-        n = N/2
-        modes = hstack((modes[-n:],modes[:n]))
+
+    modes = roll(modes,N/2)
 
     modes *= overhead[0]
 
-    #return modes
     return int_connection(modes,overhead[1])
 
 # Defines ifft_online: the overhead necessary is computed by
@@ -113,18 +95,14 @@ def fft_online(fx,overhead):
 def ifft_online(f,overhead):
 
     from numpy.fft import ifft as ift
-    from numpy import hstack
+    from numpy import roll
 
     from connection import int_connection_backward_online
 
     fx = int_connection_backward_online(f,overhead[1])
     fx /= overhead[0]
     N = fx.size
-    if bool(N%2):
-        n = (N-1)/2
-        fx = hstack((fx[-(n+1):],fx[:n]))
-    else:
-        n = N/2
-        fx = hstack((fx[-n:],fx[:n]))
+
+    fx = roll(fx,-(N/2))
 
     return ift(fx)
