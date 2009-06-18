a95f43bf7f93ab947481929098f39e4aad8ac405
Akil Narayan <anaray@dam.brown.edu>
2009-06-18 13:58:35 -0400

    eval/quad in fourier: need to debug

--- spyctral/fourier/eval.py
+++ spyctral/fourier/eval.py
@@ -2,11 +2,11 @@
 # 
 # Routines for evaluation of Fourier functions
 
-__all__ = ['fseval']
+#__all__ = ['fseval']
 
-import numpy as _np
 from scipy import pi
 
+"""
 # Evaluates the orthonormalized Fourier Series basis functions over the interval
 # [-pi*scale, pi*scale]+shift. 
 def fseval(x,ns,scale=1.,shift=0.) :
@@ -24,3 +24,144 @@ def fseval(x,ns,scale=1.,shift=0.) :
         count += 1
 
     return fs
+"""
+
+
+def fseries_eval(theta,k,g=0.,d=0.,shift=0.,scale=1.):
+    """
+    Evaluates the generalized Szego-Fourier functions at the locations theta \in 
+    [-pi,pi]. This function mods the inputs theta to lie in this interval and then
+    evaluates them. The function class is (g,d), and the function index is the
+    vector of integers k
+    """
+
+    from numpy import array, ndarray, abs, zeros
+    from numpy import sin, cos, sqrt, sign
+    from scipy import pi
+
+    from pyspec.jacobi.eval import jpoly
+    from pyspec.common.maps import standard_scaleshift as sss
+    from pyspec.common.maps import physical_scaleshift as pss
+
+    # Preprocessing: unravelling, etc.
+    theta = array(theta)
+    theta = theta.ravel()
+    sss(theta,shift=shift,scale=scale)
+    theta = (theta+pi) % (2*pi) - pi
+    if type(k) != (ndarray or list):
+        k = [k]
+    k = array(k,dtype='int')
+    k = k.ravel()
+    kneq0 = k != 0
+
+    r = cos(theta)
+
+    # Evaluate polynomials and multiplication factors
+    p1 = jpoly(r,abs(k),d-1/2.,g-1/2.).reshape([theta.size,k.size])
+
+    # Add things together
+    Psi = zeros([theta.size,k.size],dtype=complex)
+    Psi[:,~kneq0] = 1/sqrt(2)*p1[:,~kneq0]
+
+    if k[kneq0].any():
+        p2 = jpoly(r,_np.abs(k[kneq0])-1,d+1/2.,g+1/2.).\
+                reshape([theta.size,k[kneq0].size])
+        kmat = sign(k[kneq0])
+        tmat = sin(theta)
+        p2 = 1j*(p2.T*tmat).T*kmat
+        Psi[:,kneq0] = 1/2.*(p1[:,kneq0] + p2)
+
+    pss(theta,shift=shift,scale=scale)
+    return Psi.squeeze()
+
+
+def dfseries_eval(theta,k,g=0.,d=0.,shift=0.,scale=1.):
+    """
+    Evaluates the derivative of the generalized Szego-Fourier functions at the locations theta \in 
+    [-pi,pi]. This function mods the inputs theta to lie in this interval and then
+    evaluates them. The function class is (g,d), and the function index is the
+    vector of integers k
+    """
+
+    from numpy import array, zeros, dot, any, sum
+    from numpy import sin, cos, sqrt, abs, sign
+    from scipy import pi
+
+    from pyspec.jacobi.eval import jpoly, djpoly
+    from pyspec.common.maps import standard_scaleshift as sss
+    from pyspec.common.maps import physical_scaleshift as pss
+
+    # Preprocessing: unravelling, etc.
+    theta = array(theta)
+    theta = theta.ravel()
+    sss(theta,shift=shift,scale=scale)
+    # Now theta \in [-pi,pi]
+    theta = (theta+pi) % (2*pi) - pi
+    k = array(k)
+    k = k.ravel()
+    kneq0 = k != 0
+    a = d-1/2.
+    b = g-1/2.
+
+    r = _np.cos(theta)
+
+    # Term-by-term: first the even term
+    dPsi = zeros([theta.size,k.size],dtype='complex128')
+    dPsi += 1/2.*djpoly(r,abs(k),a,b)
+    dPsi = (-sin(theta)*dPsi.T).T
+    #dPsi = _np.dot(_np.diag(-_np.sin(theta)),dPsi)
+    dPsi[:,~kneq0] *= sqrt(2)
+
+    # Now the odd term:
+    if any(kneq0):
+        term2 = zeros([theta.size,sum(kneq0)],dtype=complex)
+        #term2 += _np.dot(_np.diag(_np.cos(theta)),jac.jpolyn(r,_np.abs(k[kneq0])-1,a+1,b+1))
+        term2 += (cos(theta)*jpoly(r,abs(k[kneq0])-1,a+1,b+1).T).T
+        #term2 += _np.dot(_np.diag(-_np.sin(theta)**2),jac.djpolyn(r,_np.abs(k[kneq0])-1,a+1,b+1))
+        term2 += (-sin(theta)**2*djpoly(r,abs(k[kneq0])-1,a+1,b+1).T).T
+        #term2 = 1./2*_np.dot(term2,_np.diag(1j*_np.sign(k[kneq0])))
+        term2 = 1./2*term2*(1j*sign(k[kneq0]))
+    else:
+        term2 = 0.
+
+    dPsi[:,kneq0] += term2
+
+    # Transform theta back to original interval
+    pss(theta,shift=shift,scale=scale)
+    return dPsi/scale
+
+# Evaluates the generalized weighted Szego-Fourier functions at the locations theta \in 
+# [-pi,pi]. This function mods the inputs theta to lie in this interval and then
+# evaluates them. The function class is (g,d), and the function index is the
+# vector of integers k
+def weighted_fseries(theta,k,g=0.,d=0.,shift=0.,scale=1.):
+
+    from pyspec.common.maps import standard_scaleshift as sss
+    from pyspec.common.maps import physical_scaleshift as pss
+    from numpy import sqrt, array
+    from weights import wsqrt_bias
+
+    theta = array(theta)
+    theta = theta.ravel()
+    sss(theta,shift=shift,scale=scale)
+
+    psi = fseries(theta,k,g,d,shift=0.,scale=1.)
+    phi = (wsqrt_bias(theta,g=g,d=d,shift=0.,scale=1.)*psi.T).T
+
+    pss(theta,shift=shift,scale=scale)
+    # Scaling:
+    return phi/sqrt(scale)
+
+# Evaluates the derivative of the shift/scaled weighted generalized
+# Fourier functions. 
+def dweighted_fseries(theta,ks,g=0.,d=0.,shift=0.,scale=1.):
+    from numpy import sqrt
+    from weights import wsqrt_bias, dwsqrt_bias
+
+    w = wsqrt_bias(theta,g=g,d=d,scale=scale,shift=shift)
+    dw = dwsqrt_bias(theta,g=g,d=d,scale=scale,shift=shift)
+    Psi = fseries(theta,ks,g=g,d=d,scale=scale,shift=shift)
+    dPsi = dfseries(theta,ks,g=g,d=d,scale=scale,shift=shift)
+
+    # yay product rule
+    return (dw*Psi.T + w*dPsi.T).T/sqrt(scale)
--- spyctral/fourier/quad.py
+++ spyctral/fourier/quad.py
@@ -6,9 +6,7 @@
 __all__ = ['gquad',
            'genfourier_gquad']
 
-import numpy as _np
-from scipy import pi
-
+"""
 # Returns N k-indices, using the default standard bias (to the
 # left/negative if N is even)
 def N_to_ks(N):
@@ -35,90 +33,52 @@ def gquad(N,scale=0.,shift=0.) :
     w = 2*pi/N*_np.ones(x.shape)
 
     return [x,w]
+"""
 
 # Returns generalized Szego-Fourier qudarature rules over the interval (-pi,pi).
 # The rules are constructed for the unweighted functions, and with the intent
 # that they will eventually be mapped to the real line so that we try not to
 # place nodes at x=\pm\pi
-def genfourier_gquad(N,g=0.,d=0.,shift=0.,scale=1.):
+def gq(N,g=0.,d=0.,shift=0.,scale=1.):
 
-    from spyctral.opoly1d import jacobi as jac
-    from spyctral.common import forward_scaleshift as fss
-    from spyctral.common import backward_scaleshift as bss
+    from numpy import arccos, hstack, isnan
+    from pyspec.jacobi.quad import gq as jgq
+    from pyspec.jacobi.quad import grq as jgrq
+    from pyspec.common.maps import physical_scaleshift as pss
 
     N = int(N)
     tol = 1e-8
-    #if (_np.abs(g)<tol)&(_np.abs(d)<tol):
-    #    return gquad(N)
 
     if (N%2)==0:
 
-        [r,wr] = jac.gquad(N/2,d-1/2.,g-1/2.)
+        [r,wr] = jgq(N/2,d-1/2.,g-1/2.)
         r = r.squeeze()
         wr = wr.squeeze()
-        temp = _np.arccos(r[::-1])
+        temp = arccos(r[::-1])
         wr = wr[::-1]
-        theta = _np.hstack((-temp[::-1],temp))
-        w = _np.hstack((wr[::-1],wr))
-        bss(theta,scale=scale,shift=shift)
+        theta = hstack((-temp[::-1],temp))
+        w = hstack((wr[::-1],wr))
+        pss(theta,scale=scale,shift=shift)
         return [theta,w]
 
     else:
 
-        [r,wr] = jac.grquad((N+1)/2,d-1/2.,g-1/2.,r0=1.)
+        [r,wr] = jgrq((N+1)/2,d-1/2.,g-1/2.,r0=1.)
         r = r.squeeze()
         wr = wr.squeeze()
-        temp = _np.arccos(r[::-1])
+        temp = arccos(r[::-1])
         # Silly arccos machine epsilon crap
-        temp[_np.isnan(temp)] = 0.
-        theta = _np.hstack((-temp[::-1],temp[1:]))
+        temp[isnan(temp)] = 0.
+        theta = hstack((-temp[::-1],temp[1:]))
         wr = wr[::-1]
         wr[0] *= 2
-        w = _np.hstack((wr[::-1],wr[1:]))
-        bss(theta,scale=scale,shift=shift)
+        w = hstack((wr[::-1],wr[1:]))
+        pss(theta,scale=scale,shift=shift)
         return [theta,w]
 
 # Returns the Szego-Fourier quadrature for the weighted functions
-def genfourierw_pgquad(N,g=0.,d=0.,shift=0.,scale=1.):
+def pgq(N,g=0.,d=0.,shift=0.,scale=1.):
 
-    from fourier.genfourier import wtheta
-    [theta,w] = genfourier_gquad(N,g,d,shift=shift,scale=scale)
-    return [theta,w/wtheta(theta,g,d,shift=shift,scale=scale)*scale]
-
-# Returns the modes of the input numpy array along the axis 0 assuming that the
-# nodal values are at the Szego-Fourier quadrature points. Returns modes for the
-# unweighted Szego-Fourier functions.
-# The FFT is only usable if G and D are integers; thus, this function casts them
-# as thus
-def fft(fx,G=0,D=0):
-    from fourier.connection import int_connection
-    return int_connection(fft_base(fx),0.,0.,int(G),int(D))
-
-# Returns modes of input num array along axis 0 assuming that the nodal values
-# are at the Szego-Fourier quadrature points, and that the nodal basis functions 
-# Returns modes for the weighted Szego-Fourier functions.
-def wfft(theta,fx,G=0,D=0):
-    from fourier.genfourier import wtheta_sqrt as w
-    G = int(G)
-    D = int(D)
-
-    wx = w(theta,G,D)
-    return fft(fx/wx,G,D)
-
-# Base FFT Routine: does phase shifts, etc for using canonical FFT for
-# orthonormalized canoncial Fourier basis on Szego-Fourier quadrature points
-def fft_base(fx):
-
-    from numpy import sqrt, exp
-    from numpy.fft import fft
-
-    fx = fx.squeeze()
-    N = fx.shape[0]
-    shift = -_np.array(range(N))*(1+1./N)
-    shift = exp(1j*pi*shift.T)*sqrt(2*pi)/N
-
-    #FX = fft(fx,axis=0)*shift
-    FX = _np.dot(_np.diag(shift),fft(fx,axis=0))
-    shifts = (-1)**(N+1)
-    temp = (N+1)/2
-    return _np.hstack((shifts*FX[temp:],FX[:temp]))
+    from weights import w as weight
+    [theta,w] = gq(N,g,d,shift=shift,scale=scale)
+    return [theta,w/weight(theta,g,d,shift=shift,scale=scale)*scale]
