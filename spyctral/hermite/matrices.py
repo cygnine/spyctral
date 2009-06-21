460a60970397d679392f59cb2138ca458c9cf63a
Akil Narayan <anaray@dam.brown.edu>
2009-06-21 17:44:05 -0400

    Added a sparse stiffness matrix routine to hermite/matrices

--- spyctral/hermite/matrices.py
+++ spyctral/hermite/matrices.py
@@ -17,3 +17,15 @@ def stiff_apply(modes,mu=0.,shift=0.,scale=1.):
     dmodes[1:] -= sqrt((ns[:(N-1)]+1)/2.)*modes[:(N-1)]
 
     return dmodes/scale
+
+def stiffness_matrix(N,mu=0.,shift=0.,scale=1.):
+
+    from numpy import arange,zeros,sqrt, diag
+    from scipy.sparse import csc_matrix
+
+    smat = zeros([N,N])
+
+    ns = arange(N)
+    smat += diag(sqrt(ns[1:]/2.),k=1)
+    smat -= diag(sqrt((ns[:(N-1)]+1)/2.),k=-1)
+    return csc_matrix(smat/scale)
