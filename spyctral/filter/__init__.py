1ce8ff91abbbaced75a9c4f6d77343b10b99fc11
Akil Narayan <anaray@dam.brown.edu>
2009-06-24 13:49:31 -0400

    Filter string->module parser (uugggly)

--- spyctral/filter/__init__.py
+++ spyctral/filter/__init__.py
@@ -8,3 +8,15 @@ import raised_cosine
 import cesaro
 
 #__all__ += exp.__all__
+
+def filter_parse(filter_type):
+    if filter_type == 'exponential':
+        return exponential
+    elif filter_type == 'lanczos':
+        return lanczos
+    elif filter_type == 'raised_cosine':
+        return raised_cosine
+    elif filter_type == 'cesaro':
+        return cesaro
+    else:
+        return None
