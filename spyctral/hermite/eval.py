3bbb11c654c50fbb0a47ec97ad9fd400d9c90da5
Akil Narayan <anaray@dam.brown.edu>
2009-07-15 14:37:49 -0400

    Fixed Hermite scaling
    - Used hermite/weights module in computation of functions/derivatives

--- spyctral/hermite/eval.py
+++ spyctral/hermite/eval.py
@@ -4,7 +4,7 @@
 
 # Evaluates the monic Hermite polynomials of order mu at the locations x
 def hermite_polynomial(x,n,mu=0.,d=0,shift=0.,scale=1.):
-    from numpy import max
+    from numpy import max, sqrt
     from spyctral.hermite.coeffs import recurrence_range
     from spyctral.opoly1d.eval import eval_normalized_opoly
     from spyctral.common.maps import physical_scaleshift as pss
@@ -15,12 +15,13 @@ def hermite_polynomial(x,n,mu=0.,d=0,shift=0.,scale=1.):
     sss(x,shift=shift,scale=scale)
     temp = eval_normalized_opoly(x,n,a,b,d)
     pss(x,shift=shift,scale=scale)
-    return temp
+    return temp/sqrt(scale)
 
 # Evaluates the L2 normalized Hermite functions
 def hermite_function(x,ns,mu=0.,scale=1.,shift=0.):
 
-    from numpy import array, zeros, exp, isnan, Inf
+    from numpy import array, zeros, exp, isnan, Inf, sqrt
+    from spyctral.hermite.weights import sqrt_weight, sqrt_weight
     from spyctral.common.maps import physical_scaleshift as pss
     from spyctral.common.maps import standard_scaleshift as sss
 
@@ -31,10 +32,11 @@ def hermite_function(x,ns,mu=0.,scale=1.,shift=0.):
     ps = hermite_polynomial(x,ns,mu,scale=scale,shift=shift)
     sss(x,shift=shift,scale=scale)
     #x = (x-shift)/scale
-    w = x**(mu/4)*exp(-1/2.*x**2)
+    #w = x**(mu/2)*exp(-1/2.*x**2)
+    w = sqrt_weight(x,mu=mu)
     pss(x,shift=shift,scale=scale)
     # I DON'T UNDERSTAND THIS
-    ps /= scale
+    #ps /= sqrt(scale)
     # WARNING: THIS CAUSES PROBLEMS FOR VERY VERY LARGE N: it eliminates
     # nan's
     wflags = (w==0)
@@ -44,7 +46,7 @@ def hermite_function(x,ns,mu=0.,scale=1.,shift=0.):
 # Evaluates the derivative of the L2 normalized Hermite functions
 def dhermite_function(x,ns,mu=0.,scale=1.,shift=0.):
 
-    from spyctral.hermite.weights import dsqrt_weight
+    from spyctral.hermite.weights import dsqrt_weight, sqrt_weight
     from numpy import array, zeros, exp,sqrt
     from spyctral.common.maps import physical_scaleshift as pss
     from spyctral.common.maps import standard_scaleshift as sss
@@ -57,17 +59,19 @@ def dhermite_function(x,ns,mu=0.,scale=1.,shift=0.):
     dps = hermite_polynomial(x,ns,mu,d=1,scale=scale,shift=shift)
     sss(x,shift=shift,scale=scale)
     #x = (x-shift)/scale
-    w = x**(mu)*exp(-1/2.*x**2)
+    #w = x**(mu)*exp(-1/2.*x**2)
+    w = sqrt_weight(x,mu=mu)
+    dw = dsqrt_weight(x,mu=mu)
     pss(x,shift=shift,scale=scale)
-    dw = dsqrt_weight(x,mu=mu,shift=shift,scale=scale)
+    #dw = dsqrt_weight(x,mu=mu,shift=shift,scale=scale)
 
     # yay product rule
     ps = (dps.T*w).T + (ps.T*dw).T
 
-    # I DON'T UNDERSTAND THIS (part of hfunn)
-    ps /= scale
+    # I DON'T UNDERSTAND THIS (part of hermite_function)
+    #ps /= sqrt(scale)
     # WARNING: THIS CAUSES PROBLEMS FOR VERY VERY LARGE N: it eliminates
     # nan's
     wflags = (w==0)
     ps[wflags,:] = 0.
-    return ps
+    return ps/scale
